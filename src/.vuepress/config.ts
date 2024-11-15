c4fa9e7f81982036f8ed49bad34cc2412d5df293
秋刀鱼 <9903082+qdyovo@user.noreply.gitee.com>
2024-11-15 15:55:18 +0800

    📦build: : 部署测试

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/server-docs/",
 
   lang: "zh-CN",
   title: "NikoYoke-Server",
